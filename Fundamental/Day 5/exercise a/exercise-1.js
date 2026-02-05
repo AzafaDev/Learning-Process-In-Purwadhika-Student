c46aa6f70f5329e3f25c3e4ca28f07d0e62a81b3
akmal dzakwan faiz <akmal.dz.f@gmail.com>
2026-02-05 15:41:31 +0700

    changes

--- Fundamental/Day 5/exercise a/exercise-1.js
+++ Fundamental/Day 5/exercise a/exercise-1.js
@@ -1,7 +1,7 @@
 /* 
 1. Write a function to get the lowest, highest and average value in the array (with and without sort function).
 a. Example : arr = [12, 5, 23, 18, 4, 45, 32] → {lowest : 4, highest: 45, average: 19.8xxx}
- */
+*/
 
 function getLowestHighestAverage(arr) {
   let lowest = arr[0];
--- Fundamental/Day 5/exercise a/exercise-2.js
+++ Fundamental/Day 5/exercise a/exercise-2.js
@@ -2,7 +2,7 @@
 2. Write a function that takes an array of words and returns a string by concatenating the words in the array, separated
 by commas and - the last word - by an 'and'.
 a. Example : arr = ["apple", "banana", "cherry", "date"] → “apple,banana,cherry, and date”
- */
+*/
 
 function JoinArray(arr) {
   let result = '';
