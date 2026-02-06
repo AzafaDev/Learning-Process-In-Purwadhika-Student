bfaa320feee5d5ba28b76e2bc5e1e0bfc0a3db3a
akmal dzakwan faiz <akmal.dz.f@gmail.com>
2026-02-06 10:37:20 +0700

    changes showStatus function

--- Fundamental/Day 5/exercise morning/exercise.js
+++ Fundamental/Day 5/exercise morning/exercise.js
@@ -18,21 +18,20 @@ notes
 let player1 = ['Budi', 100, 10];
 let player2 = ['Doremi', 100, 10];
 
-function showStatus(player) {
+function showStatus(player1, player2) {
   console.log(
-    `Name: ${player[0]}, Health: ${player[1] > 0 ? player[1] : 0}, Power: ${player[2]}`,
+    `Name: ${player1[0]}, Health: ${player1[1] > 0 ? player1[1] : 0}, Power: ${player1[2]} \nName: ${player2[0]}, Health: ${player2[1] > 0 ? player2[1] : 0}, Power: ${player2[2]}  `,
   );
 }
 
 function damage(player1, player2) {
-  player1[1] -= player2[2];
   player2[1] -= player1[2];
 }
 
 function randomItem() {
   let item = [0, 0];
-  let isHealth = Math.random() < 0.5 ? 0 : 10;
-  let isPower = Math.random() < 0.5 ? 0 : 10;
+  let isHealth = Math.random() < 0.5 ? 0 : 10; 
+  let isPower = Math.random() < 0.5 ? 0 : 10; 
   item[0] = isHealth;
   item[1] = isPower;
   return item;
@@ -41,19 +40,25 @@ function randomItem() {
 function useItem(player, item) {
   player[1] += item[0];
   player[2] += item[1];
+  console.log(
+    `${player[0]} mendapatkan health ${item[0]} dan power ${item[1]}. Health: ${player[1]}, Power: ${player[2]}`,
+  );
 }
 
 function start() {
-  showStatus(player1);
-  showStatus(player2);
+  showStatus(player1, player2);
   while (player1[1] > 0 && player2[1] > 0) {
     let item1 = randomItem();
     let item2 = randomItem();
     useItem(player1, item1);
     useItem(player2, item2);
     damage(player1, player2);
-    showStatus(player1);
-    showStatus(player2);
+    showStatus(player1,player2);
+    if (player1[1] <= 0 || player2[1] <= 0) {
+      break;
+    }
+    damage(player2, player1);
+    showStatus(player1, player2);
   }
   if (player1[1] <= 0) {
     console.log(`${player2[0]} win!`);
